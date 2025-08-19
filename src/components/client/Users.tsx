77c1481bd3ac8764db28b364ec61af8a6c11fc27
Chadi Monla <chadi.ishtari@gmail.com>
2025-08-20 00:54:43 +0300

    m

--- src/components/client/Users.tsx
+++ src/components/client/Users.tsx
@@ -218,7 +218,7 @@ function clearFilter(){
                 onClick={() => deleteUser(deleteInfo.selectedId)}
                 className="px-4 py-2 text-sm bg-red-600 hover:bg-red-700 text-white rounded-md transition"
               >
-                Delete
+                Deletee
               </button>
              )}
             </div>
